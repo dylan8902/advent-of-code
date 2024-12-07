e35055ed50a2ac74305ba84d91130ff367b77155
Dylan Jones <dylan.jones@bjss.com>
2024-12-07 20:42:09 +0000

    Day 7 part 2

--- 2024/day_07/src/main.rs
+++ 2024/day_07/src/main.rs
@@ -1,3 +1,4 @@
+use radix_fmt::radix_3;
 use std::fs;
 
 fn main() {
@@ -8,7 +9,8 @@ fn main() {
         .map(String::from)
         .collect();
 
-    let mut total = 0;
+    let mut part_1 = 0;
+    let mut part_2 = 0;
 
     'line: for line in lines {
         // parse each equation
@@ -20,34 +22,59 @@ fn main() {
             .into_iter()
             .map(|s| s.parse::<i64>().unwrap())
             .collect();
-        println!(
-            "{} has answer {} made up of {} numbers",
-            line,
-            answer,
-            numbers.len()
-        );
 
-        let base: i32 = 2;
-        let max = base.pow((numbers.len() - 1) as u32) - 1;
+        // part 1
+        let mut base: i32 = 2;
+        let mut max = base.pow((numbers.len() - 1) as u32) - 1;
 
         for bit in 0..=max {
-            let binary_representation = String::from(format!("{bit:0width$b}", width=numbers.len() - 1));
-            let multiply_positions: Vec<char> = binary_representation.chars().collect();
+            let binary_representation =
+                String::from(format!("{bit:0width$b}", width = numbers.len() - 1));
+            let positions: Vec<char> = binary_representation.chars().collect();
 
             let mut sum: i64 = numbers[0];
-            for i in 0..multiply_positions.len() {
-                if multiply_positions[i] == '1' {
-                    sum *= numbers[i+1];
+            for i in 0..positions.len() {
+                if positions[i] == '1' {
+                    sum *= numbers[i + 1];
                 } else {
-                    sum += numbers[i+1];
+                    sum += numbers[i + 1];
                 }
             }
             if sum == answer {
-                total += answer;
+                part_1 += answer;
+                continue 'line;
+            }
+        }
+
+        // part 2
+        base = 3;
+        max = base.pow((numbers.len() - 1) as u32) - 1;
+
+        for bit in 0..=max {
+            let trinary_representation = String::from(format!(
+                "{:0width$}",
+                radix_3(bit).to_string().parse::<i64>().unwrap(),
+                width = numbers.len() - 1
+            ));
+            let positions: Vec<char> = trinary_representation.chars().collect();
+
+            let mut sum: i64 = numbers[0];
+            for i in 0..positions.len() {
+                if positions[i] == '2' {
+                    sum = format!("{}{}", sum, numbers[i + 1]).parse::<i64>().unwrap();
+                } else if positions[i] == '1' {
+                    sum *= numbers[i + 1];
+                } else {
+                    sum += numbers[i + 1];
+                }
+            }
+            if sum == answer {
+                part_2 += answer;
                 continue 'line;
             }
         }
     }
 
-    println!("Part 1: {}", total);
+    println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part_1 + part_2);
 }
